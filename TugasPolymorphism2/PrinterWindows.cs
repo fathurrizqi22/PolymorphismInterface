9508958670f8ab992977bed7764e9acad6d6b2c3
fathurrizqi22 <rizqi14th@gmail.com>
2020-05-13 19:12:55 +0700

    insialisasi project

---
+++ TugasPolymorphism2/PrinterWindows.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasPolymorphism2
+{
+    interface IPrinterWindows
+    {
+        void show();
+        void Print();
+    }
+
+    public class PrinterWindows : IPrinterWindows
+    {
+        public void show()
+        {
+            Console.WriteLine("printer windows dimension: ");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("printer windows printing...");
+        }
+    }
+
+    public class Epson : IPrinterWindows
+    {
+        public void show()
+        {
+            Console.WriteLine("Printer Epson dimension: 10 * 11");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+    }
+
+    public class Canon : IPrinterWindows
+    {
+        public void show()
+        {
+            Console.WriteLine("Printer Canon dimension: 9.5 * 12");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Canon printer printing....");
+        }
+
+    }
+
+    public class Laserjet : IPrinterWindows
+    {
+        public void show()
+        {
+            Console.WriteLine("Printer Laser Jet dimension 12 * 12");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Laser Jet printer printing....");
+        }
+    }
+
+    
+}
---
+++ TugasPolymorphism2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TugasPolymorphism2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pilih printer: \n1. Epson\n2. Canon\n3. LaserJet");
+
+            Console.Write("\nNomor Printer [1 ... 3] : ");
+            int pilihPrinter = Convert.ToInt32(Console.ReadLine());
+
+            IPrinterWindows printer = new PrinterWindows();
+
+
+            if (pilihPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (pilihPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new Laserjet();
+            }
+
+            printer.show();
+            printer.Print();
+        }
+    }
+}
